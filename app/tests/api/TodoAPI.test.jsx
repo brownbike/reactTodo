30fd5395b90f75b36f0828ed18633846f7f08f89
Scott McKeon <scottmckeon@gmail.com>
2017-12-27 14:46:38 -0800

    Add filtering and tests

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -48,4 +48,48 @@ describe('TodoAPI', () => {
       expect(actualTodos).toEqual(todos);
     });
   });
+
+  describe('filterTodos', () => {
+    let todos = [{
+        id: 1,
+        text: 'first text',
+        completed: true
+      },
+      {
+        id: 2,
+        text: 'some second text',
+        completed: false
+      },
+      {
+        id: 3,
+        text: 'some third text',
+        completed: true
+      }];
+
+    it('should return all items if showCompleted is true', () => {
+      let filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
+    });
+
+    it('should return uncompleted items if showCompleted is false', () => {
+      let filteredTodos = TodoAPI.filterTodos(todos, false, '');
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it('should sort by completed status', () => {
+      let filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos[0].completed).toBe(false);
+    });
+
+    it('should filter todos by searchText', () => {
+      let filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
+      expect(filteredTodos.length).toBe(2);
+    });
+
+    it('should return all items if searchText is empty', () => {
+      let filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
+    });
+  });
 });
+
