89345e807db53ecac055a8effd8a71324d94a5f9
Scott McKeon <scottmckeon@gmail.com>
2018-02-10 07:56:31 -0800

    Add Test and Prod DBs

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 
 try {
   const config = {
-    apiKey: "AIzaSyCqaSx7ysrU-gXBZcnBJuqq565buYu6bOY",
-    authDomain: "react-todo-app-672b1.firebaseapp.com",
-    databaseURL: "https://react-todo-app-672b1.firebaseio.com",
-    projectId: "react-todo-app-672b1",
-    storageBucket: "react-todo-app-672b1.appspot.com",
-    messagingSenderId: "116246858917"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
   };
   firebase.initializeApp(config);
 } catch (e) {
